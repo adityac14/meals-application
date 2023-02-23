7b50b70cedae89ae6a2b4f919f28851697c19074
Aditya <adityachatto14@gmail.com>
2023-02-23 16:51:15 -0500

    Installed Axios, using Axios for TheMealDB  API

--- src/Context.js
+++ src/Context.js
@@ -1,17 +1,35 @@
-import React, {useContext} from 'react';
+import React, { useContext, useEffect } from "react";
+import axios from "axios";
 
-const AppContext = React.createContext()
+const AppContext = React.createContext();
 
 // children is a special prop and that represents whatever we have in the component
+const allMealsURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=a";
+const randomMealURL = "https://www.themealdb.com/api/json/v1/1/random.php";
 
-const AppProvider = ({children}) => {
-    return <AppContext.Provider value={{name: 'john', role: 'student'}}>
-        {children}
+const AppProvider = ({ children }) => {
+  const fetchMeals = async (url) => {
+    try {
+      const response = await axios(url);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMeals(allMealsURL);
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ name: "john", role: "student" }}>
+      {children}
     </AppContext.Provider>
-}
+  );
+};
 
 export const useGlobalContext = () => {
-    return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export {AppContext, AppProvider}
+export { AppContext, AppProvider };
