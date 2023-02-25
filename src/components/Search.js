24181dd783225d9caed653654285462031e26137
Aditya <adityachatto14@gmail.com>
2023-02-25 15:08:03 -0500

    Created Search Component UI with CSS styles

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,15 @@
+import { useState } from "react"
+import { useGlobalContext } from "../Context"
+
 const Search = () => {
-    return <h1>Search Component </h1>
+    return <header className="search-container">
+        <form>
+            <input type="text" placeholder="Type Favourite Meal" className="form-input"/>
+            <button type="submit" className="btn">Search</button>
+            <button type="button" className="btn btn-hipster">Surprise Me!</button>
+        </form>
+    </header>
+
 }
 
 export default Search
